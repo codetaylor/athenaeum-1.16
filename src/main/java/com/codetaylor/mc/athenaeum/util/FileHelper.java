dc90e6901f157943bb4b1d190fd35bdcb8b8f01c
codetaylor <jason@codetaylor.com>
2020-12-06 10:49:40 -0800

    Add json config helper

---
+++ src/main/java/com/codetaylor/mc/athenaeum/util/FileHelper.java
@@ -0,0 +1,23 @@
+package com.codetaylor.mc.athenaeum.util;
+
+import java.io.Closeable;
+import java.io.IOException;
+
+public final class FileHelper {
+
+  public static void closeSilently(Closeable closeable) {
+
+    if (closeable != null) {
+
+      try {
+        closeable.close();
+      } catch (IOException e) {
+        //
+      }
+    }
+  }
+
+  private FileHelper() {
+    //
+  }
+}
